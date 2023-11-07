42cb5438d4c4d03a8401ca32be7072050b8a86ae
ryan <ryanroberts562@gmail.com>
2023-11-07 09:09:00 +0200

    trying hono

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,36 +1,18 @@
-import { ExecuteService } from '$lib/server/executeService';
-import { fail, type Actions } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 
 export const actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
-		const file = formData.get('files') as File;
-		if (!(file as File).name || (file as File).name === 'undefined') {
-			return fail(400, {
-				error: true,
-				message: 'You must provide a file to upload'
+		try {
+			const response = await fetch(`${env.API_HOST_PROD}/convert`, {
+				method: 'POST',
+				body: formData
 			});
+			return response.json();
+		} catch (error) {
+			console.log('error', error);
+			return {};
 		}
-
-		await toHono(formData);
-		return {};
-		// const service = new ExecuteService();
-		// const { file: convertedFileBuffer, name } = await service.writeFileAndConvert(file);
-		// return {
-		// 	name,
-		// 	file: Array.from(new Uint8Array(convertedFileBuffer))
-		// };
 	}
 } satisfies Actions;
-
-async function toHono(formData: FormData) {
-	try {
-		const resp = await fetch('http://localhost:8000/convert', {
-			method: 'POST',
-			body: formData
-		});
-		console.log('RESPONSE', resp);
-	} catch (error) {
-		console.error('error', error);
-	}
-}
