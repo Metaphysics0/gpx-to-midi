c1b830197533f44ee1c66c0c9c3aa846a776f15e
ryan <ryanroberts562@gmail.com>
2023-11-10 09:34:27 +0200

    better

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,10 +5,11 @@ export const actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
 		try {
-			const response = await fetch(`${env.API_HOST_PROD}/convert`, {
+			const response = await fetch(`${env.API_HOST}/convert`, {
 				method: 'POST',
 				body: formData
 			});
+
 			return response.json();
 		} catch (error) {
 			console.log('error', error);
