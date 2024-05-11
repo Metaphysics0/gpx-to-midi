05ec8f8bab6c6628a733cb15bfe2e919df6bce2d
ryan <ryanroberts562@gmail.com>
2024-05-11 13:46:18 +0300

    feat: add loading progress radial

---
+++ src/ui/common/LoadingProgressRadial.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+	const value: number = 50; // %
+</script>
+
+<ProgressRadial value={undefined} width="w-14">{value}%</ProgressRadial>
+
+<!-- <div class='w-1'></div> -->
